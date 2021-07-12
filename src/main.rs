94fec7e3c343f72bb6954218e5955ca3dbe8ac32
qiuzhanghua <qiuzhanghua@icoud.com>
2021-07-12 16:58:59 +0800

    fix windows path of \\computername\c:\some

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,22 @@ fn main() {
             // pb.symlink_metadata()
             // println!("{}", pb.display());
             //std::fs::read_link(pb);
-            match pb.read_link() {
-                Ok(pb2) => match pb2.canonicalize() {
-                    Ok(pb3) => {
-                        print!("{}", pb3.parent().unwrap().display())
-                    }
-                    Err(_) => exit(-2),
+            match std::env::consts::FAMILY {
+                "windows" => match pb.read_link() {
+                    Ok(pb3) => print!("{}", pb3.parent().unwrap().display()),
+                    Err(_) => print!("{}", pb.parent().unwrap().display()),
                 },
-                Err(_) => match pb.canonicalize() {
-                    Ok(pb3) => {
-                        print!("{}", pb3.parent().unwrap().display())
-                    }
-                    Err(_) => exit(-2),
+                _ => match pb.read_link() {
+                    Ok(pb2) => match pb2.canonicalize() {
+                        Ok(pb3) => print!("{}", pb3.parent().unwrap().display()),
+
+                        Err(_) => exit(-2),
+                    },
+                    Err(_) => match pb.canonicalize() {
+                        Ok(pb3) => print!("{}", pb3.parent().unwrap().display()),
+
+                        Err(_) => exit(-2),
+                    },
                 },
             }
         }
